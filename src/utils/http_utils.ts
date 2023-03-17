81741ebfea23bd66f50254eb9ab902f362f21642
Eliot Fagnon <efagnon2002@gmail.com>
2023-03-17 19:27:31 +0100

    function for retrieving all todos

---
+++ src/utils/http_utils.ts
@@ -0,0 +1,17 @@
+const BASE_URL = "127.0.0.1:5000/api/v1"
+
+export interface Todo {
+  id: number;
+  title: string;
+  description: string;
+  deadline: Date;
+  remind: Date;
+  createdAt: Date;
+  updatedAt: Date;
+
+}
+
+export async function getAll(): Promise<Todo[]> {
+  let res = await fetch(`${BASE_URL}/todos`);
+  return await res.json();
+}
