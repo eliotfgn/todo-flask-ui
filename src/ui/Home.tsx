34b71c15a06667dce2a2546e23f7e1ecd498facd
Eliot Fagnon <efagnon2002@gmail.com>
2023-03-17 19:55:21 +0100

    add button for new task

--- src/ui/Home.tsx
+++ src/ui/Home.tsx
@@ -15,13 +15,16 @@ function Home() {
 
 
   return (
-      <div className="grid grid-cols-2 md:grid-cols-4 mx-auto w-9/12 justify-around items-center gap-x-8 gap-y-4">
-        { todos?.map((todo, index) => (
-            <div key={index} className={` rounded-md bg-red-300 p-4`}>
-              <h1 className={"font-bold"}>{todo.title}</h1>
-              <p className={"text-sm"}>{todo.description}</p>
-            </div>
-        )) }
+      <div className={"py-5 flex flex-col"}>
+        <button className={"my-5 py-2 px-4 bg-blue-400 rounded-md text-white self-end mx-10"}>New task</button>
+        <div className="grid grid-cols-2 md:grid-cols-4 mx-auto w-9/12 justify-around items-center gap-x-8 gap-y-4">
+          {todos?.map((todo, index) => (
+              <div key={index} className={` rounded-md bg-red-300 p-4`}>
+                <h1 className={"font-bold"}>{todo.title}</h1>
+                <p className={"text-sm"}>{todo.description}</p>
+              </div>
+          ))}
+        </div>
       </div>
   );
 }
